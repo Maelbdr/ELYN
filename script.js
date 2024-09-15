a8319a82032353aa7724df6ef15c9a8fd7c562e6
Maelbdr <120skyman@gmail.com>
2024-09-15 15:47:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script pour l'animation du logo
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.logo-animation img');
+    // Animation supplémentaire pour le logo si besoin
+});
